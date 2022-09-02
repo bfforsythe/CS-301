0809b9a50bd7152f26212e5f29dc3b49ff0d4e60
bfforsythe <bfforsythe@alaska.edu>
2022-09-02 13:24:58 -0800

    created and staged both .h and .cpp

--- ToD Project/timeofday.h
+++ ToD Project/timeofday.h
@@ -1,23 +1,17 @@
+// timeofday.h
+// Brandon Forsythe
+// 2022-09-02
+// Header with Class for timeofday.cpp
+
 #pragma once
 
 #ifndef timeofday
 
 
-
-class TimeOfDay {
-
-
-
-
-};
-
-
-
-
-
-
+class TimeOfDay
 
 
 
 
 #endif // !timeofday
+
